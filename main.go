041f89172a41f0bbe0111581eb45db878207e396
Weilence <weilence@163.com>
2024-03-19 20:17:10 +0800

    feat: support script and token

--- main.go
+++ main.go
@@ -2,69 +2,18 @@ package main
 
 import (
 	"flag"
-	"io"
-	"log"
-	"log/slog"
-	"net/http"
-	"os"
-	"os/signal"
-	"strings"
 
-	"github.com/goccy/go-yaml"
+	"github.com/weilence/proxy-converter/internal"
 )
 
 var (
-	addr = flag.String("addr", "127.0.0.1:8080", "server address")
+	addr   = flag.String("addr", "127.0.0.1:8080", "server address")
+	script = flag.String("script", "", "script")
+	token  = flag.String("token", "", "token")
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /convert", func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Query().Get("url")
+	flag.Parse()
 
-		res, err := http.Get(url)
-		if err != nil {
-			panic(err)
-		}
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			panic(err)
-		}
-		defer res.Body.Close()
-
-		var data map[string]interface{}
-		err = yaml.Unmarshal(body, &data)
-		if err != nil {
-			panic(err)
-		}
-
-		rules := data["rules"].([]any)
-		for i, rule := range rules {
-			rule := rule.(string)
-			rules[i] = strings.ReplaceAll(rule, ",,", ",")
-		}
-
-		config, err := yaml.Marshal(data)
-		if err != nil {
-			panic(err)
-		}
-
-		w.Header().Set("Content-Disposition", "attachment; filename=config.yaml")
-		w.Header().Set("Content-Type", "application/x-yaml")
-		w.Write(config)
-	})
-
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-
-	go func() {
-		slog.Info("Server is running", "addr", *addr)
-		if err := http.ListenAndServe(*addr, mux); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	<-signalChan
-	slog.Info("Shutting down...")
+	internal.Run(*addr, *token, *script)
 }
