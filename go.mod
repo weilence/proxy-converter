6c2e16fee4a96965dad5b9cbdb50849e1f5a8f3f
Weilence <weilence@163.com>
2025-05-25 15:53:03 +0800

    feat: allow expand proxy-providers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/weilence/proxy-converter
 
 go 1.24.3
+
+require github.com/goccy/go-yaml v1.17.1
--- internal/server.go
+++ internal/server.go
@@ -1,11 +1,15 @@
 package internal
 
 import (
+	"io"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
+
+	"github.com/goccy/go-yaml"
 )
 
 func Run(addr string, token string, configPath string) {
@@ -19,9 +23,134 @@ func Run(addr string, token string, configPath string) {
 			return
 		}
 
+		var config map[string]any
+		err = yaml.Unmarshal(content, &config)
+		if err != nil {
+			slog.Error("Failed to unmarshal config", "error", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		expand := r.URL.Query().Get("expand") == "1"
+		if expand {
+			proxyProviders, ok := config["proxy-providers"].(map[string]any)
+			if ok {
+				proxyProvidersCache := make(map[string][]string)
+				for proxyProviderName, proxyProvider := range proxyProviders {
+					proxyProviderMap, ok := proxyProvider.(map[string]any)
+					if !ok {
+						continue
+					}
+
+					proxyProviderType, ok := proxyProviderMap["type"].(string)
+					if !ok {
+						continue
+					}
+
+					if proxyProviderType != "http" {
+						continue
+					}
+
+					proxyProviderUrl, ok := proxyProviderMap["url"].(string)
+					if !ok {
+						continue
+					}
+
+					proxyProviderContent, err := downloadFile(proxyProviderUrl)
+					if err != nil {
+						slog.Error("Failed to download file", "error", err)
+						http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+						return
+					}
+
+					var proxyProviderConfig map[string]any
+					err = yaml.Unmarshal(proxyProviderContent, &proxyProviderConfig)
+					if err != nil {
+						slog.Error("Failed to unmarshal proxy provider config", "error", err)
+						http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+						return
+					}
+
+					proxies, ok := config["proxies"].([]any)
+					if !ok {
+						continue
+					}
+
+					downloadProxies, ok := proxyProviderConfig["proxies"].([]any)
+					if !ok {
+						continue
+					}
+
+					var excludeFilter string
+					if excludeFilter, ok = proxyProviderMap["exclude-filter"].(string); !ok {
+						excludeFilter = ""
+					}
+
+					for _, downloadProxy := range downloadProxies {
+						downloadProxyMap, ok := downloadProxy.(map[string]any)
+						if !ok {
+							continue
+						}
+
+						downloadProxyName, ok := downloadProxyMap["name"].(string)
+						if !ok {
+							continue
+						}
+
+						if excludeFilter != "" && strings.Contains(downloadProxyName, excludeFilter) {
+							continue
+						}
+
+						proxyProvidersCache[proxyProviderName] = append(proxyProvidersCache[proxyProviderName], downloadProxyName)
+						proxies = append(proxies, downloadProxy)
+					}
+
+					config["proxies"] = proxies
+				}
+
+				delete(config, "proxy-providers")
+
+				proxyGroups, ok := config["proxy-groups"].([]any)
+				if ok {
+					for _, proxyGroup := range proxyGroups {
+						proxyGroupMap, ok := proxyGroup.(map[string]any)
+						if !ok {
+							continue
+						}
+
+						proxyGroupUse, ok := proxyGroupMap["use"].([]any)
+						if !ok {
+							continue
+						}
+
+						for _, proxyGroupName := range proxyGroupUse {
+							if proxies, ok := proxyProvidersCache[proxyGroupName.(string)]; ok {
+								ps, ok := proxyGroupMap["proxies"].([]any)
+								if !ok {
+									proxyGroupMap["proxies"] = proxies
+								} else {
+									for _, proxy := range proxies {
+										ps = append(ps, proxy)
+									}
+									proxyGroupMap["proxies"] = ps
+								}
+							}
+						}
+						delete(proxyGroupMap, "use")
+					}
+				}
+			}
+		}
+
+		bs, err := yaml.Marshal(config)
+		if err != nil {
+			slog.Error("Failed to marshal config", "error", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 		w.Header().Set("Content-Disposition", "attachment; filename=config.yaml")
 		w.Header().Set("Content-Type", "application/x-yaml")
-		w.Write(content)
+		w.Write(bs)
 	}))
 
 	signalChan := make(chan os.Signal, 1)
@@ -48,4 +177,14 @@ func tokenMiddleware(token string, next http.HandlerFunc) http.HandlerFunc {
 
 		next(w, r)
 	}
+}
+
+func downloadFile(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
 }
